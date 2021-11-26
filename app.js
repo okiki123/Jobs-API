caa6830f442d8b135ea2b81d527c015a4a26c18b
okiki123 <okikimalomo17@gmail.com>
2021-11-26 14:02:16 +0100

    Updated Commit

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect.js");
+
+const authRouther = require("./routes/auth.js");
+const jobsRouther = require("./routes/jobs.js");
+
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use("/api/v1/auth", authRouther);
+app.use("api/v1/jobs", jobsRouther);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs')
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+});
+
+
+UserSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt)
+})
+
+UserSchema.methods.createJWT = function() {
+    return jwt.sign({userID:this._id,name:this.name}, process.env.JWT_SECRET, {
+        expiresIN: process.env.JWT_LIFETIME
+    })
+}
+module.exports = mongoose.model('User', UserSchema)
